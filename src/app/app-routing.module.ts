cc896b46644c3265ba50281be4522f8763f60ac3
Patcharaphon Samakun <Patcharaphon Samakun@MSI>
2024-01-16 14:00:53 +0700

    Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
